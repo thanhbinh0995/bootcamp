405af5bac07ff54589e800ff627191da0b8e785e
thanhbinh095 <thanhbinh0995@gmail.com>
2016-10-22 15:56:51 +0700

    auto bar scroll

--- creative/js/main.js
+++ creative/js/main.js
@@ -21,23 +21,34 @@ jQuery(document).ready(function($) {
 	        i.find(".active").removeClass("active"), $(this).addClass("active")
 	    })
 	})
-
-	$('.counter-num').each(function () {
-	    $(this).prop('Counter',0).animate({
-	        Counter: $(this).text()
-	    }, {
-	        duration: 5000,
-	        easing: 'swing',
-	        step: function (now) {
-	            $(this).text(Math.ceil(now));
-	        }
-	    });
+	var $section = $('#counter');
+	$(document).bind('scroll', function(ev) {
+		var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        $('.counter-num').each(function () {
+		    $(this).prop('Counter',0).animate({
+		        Counter: $(this).text()
+		    }, {
+		        duration: 5000,
+		        easing: 'swing',
+		        step: function (now) {
+		            $(this).text(Math.ceil(now));
+		        }
+		    });
+		});
 	});
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
 
+ 	$section = $('#specialization');
+	$(document).bind('scroll', function(ev) {
+		var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+			jQuery('.skillbar').find('.skillbar-bar').animate({
+				width:jQuery('.skillbar').attr('data-percent')
+			},6000);
+			$(document).unbind('scroll');
+		}
+	});
 });
+
 
