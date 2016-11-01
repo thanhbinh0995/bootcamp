1432af404b752352446860c15466fa2fd2e21e71
thanhbinh095 <thanhbinh0995@gmail.com>
2016-11-01 16:40:34 +0700

    update sth

---
+++ trulife/js/form_validation.js
@@ -0,0 +1,89 @@
+jQuery(document).ready(function($) {
+	$( "#signupForm" ).validate( {
+		rules: {
+			companyName: {
+				required: true,
+				maxlength: 25
+			},
+			Name: {
+				required: true,
+				maxlength: 25
+			},
+			emailAddress: {
+				required: true,
+			},
+			phoneNumber: {
+				required: true,
+				number: true,
+				matches1: true
+			},
+			subject: {
+				required: true,
+				maxlength: 25
+			},
+			message: {
+				required: true
+			}
+		}
+		// messages: {
+		// 	companyName: {
+		// 		required: "Vui lòng nhập vào đây",
+		// 		maxlength: "Tên đăng nhập tối đa 25 ký tự",
+		// 	},
+		// 	Name: {
+		// 		required: "Vui lòng nhập vào đây",
+		// 		maxlength: "Tên đăng nhập tối đa 25 ký tự",
+		// 	},
+		// 	emailAddress:{
+		// 		required: "Vui lòng nhập vào đây"
+		// 	}.
+		// 	phoneNumber:{
+		// 		required: "Vui lòng nhập vào đây",
+		// 		matches1: "nhap dung so dien thoai"
+		// 	},
+		// 	subject: {
+		// 		required: "Vui lòng nhập vào đây",
+		// 		maxlength: "Tên đăng nhập tối đa 25 ký tự",
+		// 	},
+		// 	message: {
+		// 		required: "Vui lòng nhập vào đây"
+		// 	}
+		// },
+		
+	});
+
+	$('#signupForm')
+        .find('[name="phoneNumber"]')
+            .intlTelInput({
+                utilsScript: '/vendor/intl-tel-input/lib/libphonenumber/build/utils.js',
+                autoPlaceholder: true,
+                preferredCountries: ['fr', 'us', 'gb']
+            });
+
+    $('#signupForm')
+        .formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                phoneNumber: {
+                    validators: {
+                        callback: {
+                            message: 'The phone number is not valid',
+                            callback: function(value, validator, $field) {
+                                return value === '' || $field.intlTelInput('isValidNumber');
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        // Revalidate the number when changing the country
+        .on('click', '.country-list', function() {
+            $('#signupForm').formValidation('revalidateField', 'phoneNumber');
+        });
+	
+});
