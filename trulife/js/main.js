3880be68ccc76eae7072ecbc1f6afdc430c66b93
thanhbinh095 <thanhbinh0995@gmail.com>
2016-11-07 17:53:04 +0700

    haha

--- trulife/js/main.js
+++ trulife/js/main.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
-	 $(".nav-tabs a").click(function(){
-        $(this).tab('show');
+	$(".nav-tabs a").click(function(){
+    	$('#change').text($(this).text());
+		$(this).tab('show');
     });
+
+	// $(function() {
+	//     /* initiate plugin */
+	//     $("div.holder").jPages({
+	//         containerID : "itemContainer",
+	//         callback    : function( pages,
+	//         items ){
+	//             $("#legend1").html("Page " + pages.current + " of " + pages.count);
+	//         $("#legend2").html(items.range.start + " - " + items.range.end + " of " + items.count);
+	//         }
+	//     });
+	// });
 });
